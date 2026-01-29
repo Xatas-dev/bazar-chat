4928f514dcec679e61461a25a2898885081c2d1b
elSasen <seny-pnz2003@mail.ru>
2026-01-30 02:02:47 +0300

    BZR-26 Добавить к топику /topic/chat/chatId типы событий

---
+++ src/main/java/org/bazar/chat/adapter/outbound/websocket/MessageWebSocketClient.java
@@ -0,0 +1,24 @@
+package org.bazar.chat.adapter.outbound.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.bazar.chat.app.api.message.MessageEventsService;
+import org.bazar.chat.app.api.message.dto.event.ChatEvent;
+import org.bazar.chat.app.api.message.dto.event.MessageCreatedEvent;
+import org.bazar.chat.app.api.message.dto.event.MessageDeletedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageWebSocketClient implements MessageEventsService {
+    private final MessageWebSocketSender messageWebSocketSender;
+    private final WebSocketMessageMapper mapper;
+
+    @Override
+    public void publishEvent(ChatEvent event) {
+        // думаю, что добавлю сюда Стратегию, но через switch как будто читабелньей так что хз
+        switch (event.getType()) {
+            case CREATED -> messageWebSocketSender.send(mapper.map((MessageCreatedEvent) event));
+            case DELETED -> messageWebSocketSender.send(mapper.map((MessageDeletedEvent) event));
+        }
+    }
+}
---
+++ src/main/java/org/bazar/chat/app/api/message/dto/event/ChatEventType.java
@@ -0,0 +1,6 @@
+package org.bazar.chat.app.api.message.dto.event;
+
+public enum ChatEventType {
+    CREATED,
+    DELETED
+}
