dc85da5088ce258380b216524a605e7e1bc088d5
elSasen <seny-pnz2003@mail.ru>
2026-05-26 23:12:02 +0300

    BZR-137 Эндпоинт PUT /chats/{chatId}/messages/{messageId}/reactions/{reactionId} и новое событие в WS

---
+++ src/main/java/org/bazar/chat/adapter/inbound/rest/reaction/ReactionController.java
@@ -0,0 +1,21 @@
+package org.bazar.chat.adapter.inbound.rest.reaction;
+
+import lombok.RequiredArgsConstructor;
+import org.bazar.chat.api.ReactionsApi;
+import org.bazar.chat.app.api.reaction.UpdateMessageReactionInbound;
+import org.bazar.chat.app.api.reaction.dto.UpdateReactionDto;
+import org.bazar.chat.model.ReactionUpdateResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReactionController implements ReactionsApi {
+    private final UpdateMessageReactionInbound updateMessageReactionInbound;
+
+    @Override
+    public ResponseEntity<ReactionUpdateResponse> updateMessageReaction(Long chatId, Long messageId, Long reactionId) {
+        UpdateReactionDto result = updateMessageReactionInbound.execute(chatId, messageId, reactionId);
+        return ResponseEntity.ok().body(new ReactionUpdateResponse(result.messageId(), result.reactionId(), result.count()));
+    }
+}
