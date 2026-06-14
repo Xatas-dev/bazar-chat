4838517a5638cc7a5ffaf86c6f6a5a4da0d52c93
paul <kulebyakinpaul@yandex.ru>
2026-06-14 17:56:03 +0300

    BZR-145 Сделал ребейз

--- src/main/java/org/bazar/chat/adapter/inbound/rest/reaction/RestReactionMapper.java
+++ src/main/java/org/bazar/chat/adapter/inbound/rest/reaction/RestReactionMapper.java
@@ -2,13 +2,10 @@ package org.bazar.chat.adapter.inbound.rest.reaction;
 
 import org.bazar.chat.adapter.inbound.rest.reaction.dto.V1MessageReactionListResponse;
 import org.bazar.chat.adapter.inbound.rest.reaction.dto.V1ReactionUpdateResponse;
+import org.bazar.chat.adapter.inbound.rest.reaction.dto.V1ReactionUpdateResponse.V1UpdatedReaction;
 import org.bazar.chat.app.api.reaction.dto.MessageReactionListDto;
 import org.bazar.chat.app.api.reaction.dto.UpdatedReactionDto;
 import org.bazar.chat.app.api.reaction.dto.UpdatedReactionsDto;
-import org.bazar.chat.model.MessageReactionListResponse;
-import org.bazar.chat.model.ReactionUpdateResponse;
-import org.bazar.chat.model.UpdatedReaction;
-import org.bazar.chat.app.api.reaction.dto.UpdateReactionDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -17,10 +14,10 @@ public interface RestReactionMapper {
     V1MessageReactionListResponse toV1MessageReactionListResponse(MessageReactionListDto messageReactionList);
 
     @Mapping(target = "messageId", source = "messageId")
-    @Mapping(target = "updatedReactions", source = "updatedReactions")
-    ReactionUpdateResponse toV1ReactionUpdateResponse(UpdatedReactionsDto updatedReactionsDto);
+    @Mapping(target = "reactions", source = "updatedReactions")
+    V1ReactionUpdateResponse toV1ReactionUpdateResponse(UpdatedReactionsDto updatedReactionsDto);
 
-    UpdatedReaction toUpdatedReaction(UpdatedReactionDto updatedReactionDto);
+    V1UpdatedReaction toUpdatedReaction(UpdatedReactionDto updatedReactionDto);
 
     default String map(Long value) { return value == null ? null : value.toString(); }
 }
--- src/main/java/org/bazar/chat/adapter/inbound/rest/reaction/dto/V1ReactionUpdateResponse.java
+++ src/main/java/org/bazar/chat/adapter/inbound/rest/reaction/dto/V1ReactionUpdateResponse.java
@@ -1,8 +1,15 @@
 package org.bazar.chat.adapter.inbound.rest.reaction.dto;
 
+import java.util.List;
+
 public record V1ReactionUpdateResponse(
         String messageId,
-        String reactionId,
-        Long count
+        List<V1UpdatedReaction> reactions
 ) {
+
+    public record V1UpdatedReaction(
+            String reactionId,
+            Long count
+    ) {
+    }
 }
